77beec6df96e3e98dc696f6eb2296aac57a85736
Christie Du <christie.du.w@gmail.com>
2022-03-14 16:42:46 -0400

    Update data_augmentation

--- lib/data_augmentation_utils.py
+++ lib/data_augmentation_utils.py
@@ -9,34 +9,35 @@ Created on Sat Mar 12 11:59:22 2022
 import imageio
 import imgaug as ia
 import imgaug.augmenters as iaa
+import random
 
 
-def hflip(img):    
-    flip_hr=iaa.Fliplr(p=1.0)
-    flip_hr_image= flip_hr.augment_image(img)
-    return flip_hr_image
+ANGLES = [-90, -180, -270]
 
-def vflip(img):
-    flip_vr=iaa.Flipud(p=1.0)
-    flip_vr_image= flip_vr.augment_image(img)
-    return flip_vr_image
+def flip(img):     
+    choice = random.randint(0,1)
+    if choice == 0:
+        flip_hr=iaa.Fliplr(p=1.0)
+        flip_hr_image= flip_hr.augment_image(img)
+        return flip_hr_image
+    else:
+        flip_vr=iaa.Flipud(p=1.0)
+        flip_vr_image= flip_vr.augment_image(img)
+        return flip_vr_image
 
 def rotate(img):
-    rotate1=iaa.Affine(rotate=(-90, -90))
-    rotate2=iaa.Affine(rotate=(-180, -180))
-    rotate3=iaa.Affine(rotate=(-270, -270))
-
-    rotated_image1=rotate1.augment_image(img)
-    rotated_image2=rotate2.augment_image(img)
-    rotated_image3=rotate3.augment_image(img)
+    choice = random.randint(0,2)
+    angle = ANGLES[choice]
     
-    return rotated_image1, rotated_image2, rotated_image3
+    rotate1=iaa.Affine(rotate=(angle, angle))
+    rotated_image1=rotate1.augment_image(img)
+    
+    return rotated_image1
 
 def distort(img):   
     aug = iaa.PerspectiveTransform(scale=(0.01, 0.15))
     distort_image1 = aug.augment_image(img)
-    distort_image2 = aug.augment_image(img)
-    return distort_image1, distort_image2
+    return distort_image1
 
 def crop(img):
     crop = iaa.Crop(percent=(0, 0.3)) # crop image
@@ -44,11 +45,9 @@ def crop(img):
     return crop_image
 
 def brightness(img):
-    contrast1=iaa.GammaContrast(gamma=(0.5, 1.0))
-    contrast2=iaa.GammaContrast(gamma=(1.0, 2.0))
-    lighter = contrast1.augment_image(img)
-    darker = contrast2.augment_image(img)
-    return lighter, darker
+    contrast = iaa.GammaContrast(gamma=(0.5, 1.5))
+    contrast_image = contrast.augment_image(img)
+    return contrast_image
 
 def color(img):
     aug = iaa.GammaContrast((0.5, 2.0), per_channel=True)
@@ -56,31 +55,21 @@ def color(img):
     return color_img
 
 
-image = imageio.imread("../data/train_data/images/00001.png")
+'''image = imageio.imread("../data/train_data/images/00001.png")
 ia.imshow(image)
 
-flip_hr = hflip(image)
-ia.imshow(flip_hr)
+flip_image = flip(image)
+ia.imshow(flip_image)
 
-flip_vr = vflip(image)
-ia.imshow(flip_vr)
-
-rotated1, rotated2, rotated3 = rotate(image)
+rotated1 = rotate(image)
 ia.imshow(rotated1)
-ia.imshow(rotated2)
-ia.imshow(rotated3)
 
-distorted1, distorted2 = distort(image)
+distorted1 = distort(image)
 ia.imshow(distorted1)
-ia.imshow(distorted2)
 
-cropped = crop(image)
-ia.imshow(cropped)
-
-light, dark = brightness(image)
-ia.imshow(dark)
+light = brightness(image)
 ia.imshow(light)
 
 colored = color(image)
-ia.imshow(colored)
+ia.imshow(colored)'''
 
